18db7097844ecd1d8002908691f96d6f3df88d77
Eumericano <opwer032@naver.com>
2021-08-04 22:33:55 +0900

    style: Add appear animation

--- younghyun/portfolio/src/components/nav/menu.tsx
+++ younghyun/portfolio/src/components/nav/menu.tsx
@@ -18,23 +18,34 @@ interface propsIState {
 }
 
 const appear = keyframes`
-    
-`;
+    0%{ 
+        transform: translate3d(100%, 0, 0);
+    }
+    90% {
+        transform: translate3d(20%, 0, 0);
+    }
+    100% {
+        transform: translate3d(0%, 0, 0);
+    }
+    `;
 
 const Container = styled.div`
     width: 100%;
     height: 100%;
-
+    
     position: fixed;
     top: 0;
     left: 0;
-
+    
     display: flex;
     justify-content: center;
     align-items: center;
-
+    
     background-color: ${({ theme }: { theme: ThemeIState }) => theme.primary};
     color: ${({ theme }: { theme: ThemeIState }) => theme.fourth};
+    
+    transform: translate3d(-100%, 0, 0);
+    animation: ${appear} 0.3s linear forwards;
 
     z-index: 99;
 `;
