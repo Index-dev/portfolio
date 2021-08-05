0ad534dad75c33bc53acad27c1b1d6c6cb0b50d2
Eumericano <opwer032@naver.com>
2021-08-05 20:20:34 +0900

    fix: Check pageYOffset when container position is absolute

--- younghyun/portfolio/src/components/nav/index.tsx
+++ younghyun/portfolio/src/components/nav/index.tsx
@@ -2,122 +2,125 @@ import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 function Navigation(props: propsIState) {
-    const { toggleMenu } = props
-    const navRef = useRef<HTMLDivElement>(null);
-    const topPathRef = useRef<SVGPathElement>(null);
-    const middlePathRef = useRef<SVGPathElement>(null);
-    const bottomPathRef = useRef<SVGPathElement>(null);
+  const { toggleMenu } = props;
+  const navRef = useRef<HTMLDivElement>(null);
+  const topPathRef = useRef<SVGPathElement>(null);
+  const middlePathRef = useRef<SVGPathElement>(null);
+  const bottomPathRef = useRef<SVGPathElement>(null);
 
-    useEffect(() => {
-        document.addEventListener("scroll", () => {
-            if (navRef.current) {
-                const stickyPoint = window.innerHeight * 0.03;
-                const absolutePoint = window.innerHeight * 0.08;
-                if (stickyPoint <= window.pageYOffset) {
-                    navRef.current.style.position = "fixed";
-                    navRef.current.style.top = "3vh";
-                } else if (absolutePoint >= window.pageYOffset) {
-                    navRef.current.style.position = "absolute";
-                    navRef.current.style.top = "8vh";
-                }
-            }
-        });
-    });
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollEvent, true);
+  });
 
-    useEffect(() => {
-        if (navRef.current) {
-            navRef.current.addEventListener("mouseover", () => {
-                if (topPathRef.current) {
-                    topPathRef.current.style.opacity = "0.9";
-                    topPathRef.current.style.strokeWidth = "6";
-                }
-                if (middlePathRef.current) {
-                    middlePathRef.current.style.strokeDasharray =
-                        "0 17 10.5 17";
-                    middlePathRef.current.style.strokeWidth = "4";
-                }
-                if (bottomPathRef.current) {
-                    bottomPathRef.current.style.opacity = "0.9";
-                    bottomPathRef.current.style.strokeWidth = "6";
-                }
-            });
-            navRef.current.addEventListener("mouseout", () => {
-                if (topPathRef.current) {
-                    topPathRef.current.style.opacity = "0.7";
-                    topPathRef.current.style.strokeWidth = "5";
-                }
-                if (middlePathRef.current) {
-                    middlePathRef.current.style.strokeDasharray = "0 0 44.5 0";
-                    middlePathRef.current.style.strokeWidth = "5";
-                }
-                if (bottomPathRef.current) {
-                    bottomPathRef.current.style.opacity = "0.7";
-                    bottomPathRef.current.style.strokeWidth = "5";
-                }
-            });
+  function handleScrollEvent() {
+    if (navRef.current) {
+      const stickyPoint = window.innerHeight * 0.03;
+      const absolutePoint = window.innerHeight * 0.08;
+      if (stickyPoint <= window.pageYOffset) {
+        console.log("fixed");
+        navRef.current.style.position = "fixed";
+        navRef.current.style.top = "3vh";
+      } else if (absolutePoint >= window.pageYOffset) {
+        console.log("absolute");
+        navRef.current.style.position = "absolute";
+        navRef.current.style.top = "8vh";
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (navRef.current) {
+      navRef.current.addEventListener("mouseover", () => {
+        if (topPathRef.current) {
+          topPathRef.current.style.opacity = "0.9";
+          topPathRef.current.style.strokeWidth = "6";
         }
-    }, []);
+        if (middlePathRef.current) {
+          middlePathRef.current.style.strokeDasharray = "0 17 10.5 17";
+          middlePathRef.current.style.strokeWidth = "4";
+        }
+        if (bottomPathRef.current) {
+          bottomPathRef.current.style.opacity = "0.9";
+          bottomPathRef.current.style.strokeWidth = "6";
+        }
+      });
+      navRef.current.addEventListener("mouseout", () => {
+        if (topPathRef.current) {
+          topPathRef.current.style.opacity = "0.7";
+          topPathRef.current.style.strokeWidth = "5";
+        }
+        if (middlePathRef.current) {
+          middlePathRef.current.style.strokeDasharray = "0 0 44.5 0";
+          middlePathRef.current.style.strokeWidth = "5";
+        }
+        if (bottomPathRef.current) {
+          bottomPathRef.current.style.opacity = "0.7";
+          bottomPathRef.current.style.strokeWidth = "5";
+        }
+      });
+    }
+  }, []);
 
-    return (
-        <Container ref={navRef} onClick={toggleMenu}>
-            <SVG viewBox="0 0 45 40" xmlns="http://www.w3.org/2000/svg">
-                <Path d="M0 2.5H44.5" ref={topPathRef} />
-                <Path
-                    d="M0 19.5H44.5"
-                    ref={middlePathRef}
-                    strokeDasharray="0 0 44.5 0"
-                />
-                <Path d="M0 37.5H44.5" ref={bottomPathRef} />
-            </SVG>
-        </Container>
-    );
+  return (
+    <Container ref={navRef} onClick={toggleMenu}>
+      <SVG viewBox="0 0 45 40" xmlns="http://www.w3.org/2000/svg">
+        <Path d="M0 2.5H44.5" ref={topPathRef} />
+        <Path
+          d="M0 19.5H44.5"
+          ref={middlePathRef}
+          strokeDasharray="0 0 44.5 0"
+        />
+        <Path d="M0 37.5H44.5" ref={bottomPathRef} />
+      </SVG>
+    </Container>
+  );
 }
 
 export default Navigation;
 
 interface propsIState {
-    toggleMenu: () => void;
+  toggleMenu: () => void;
 }
 
 const Container = styled.div`
-    min-width: 40px;
-    width: 5vw;
-    max-width: 50px;
+  min-width: 40px;
+  width: 5vw;
+  max-width: 50px;
 
-    min-height: 40px;
-    height: 5vw;
-    max-height: 50px;
+  min-height: 40px;
+  height: 5vw;
+  max-height: 50px;
 
-    position: absolute;
-    top: 8vh;
-    right: 5vw;
+  position: absolute;
+  top: 8vh;
+  right: 5vw;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    cursor: pointer;
+  cursor: pointer;
 `;
 
 const SVG = styled.svg`
-    width: 70%;
-    height: 70%;
+  width: 70%;
+  height: 70%;
 
-    fill: ${({ theme }: { theme: ThemeIState }) => theme.primary};
+  fill: ${({ theme }: { theme: ThemeIState }) => theme.primary};
 
-    transition: all 0.4s linear;
+  transition: all 0.4s linear;
 
-    &:hover {
-        transform: rotateX(180deg);
-    }
+  &:hover {
+    transform: rotateX(180deg);
+  }
 `;
 
 const Path = styled.path`
-    stroke: ${({ theme }: { theme: ThemeIState }) => theme.primary};
-    stroke-width: 5;
+  stroke: ${({ theme }: { theme: ThemeIState }) => theme.primary};
+  stroke-width: 5;
 
-    opacity: 0.7;
-    transition: all 0.2s linear;
+  opacity: 0.7;
+  transition: all 0.2s linear;
 
-    transform: translate3d(0, 10, 0);
+  transform: translate3d(0, 10, 0);
 `;
