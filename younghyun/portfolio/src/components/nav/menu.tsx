5fd4c441bf78bac509adfe637f8ee8063f732806
EumYoungHyun <opwer032@naver.com>
2021-08-08 14:48:07 +0900

    feat: Change menu slide

--- younghyun/portfolio/src/components/nav/menu.tsx
+++ younghyun/portfolio/src/components/nav/menu.tsx
@@ -1,73 +1,84 @@
-import React from 'react';
-import styled, { css, keyframes } from 'styled-components';
+import React from "react";
+import styled, { css, keyframes } from "styled-components";
 
 function Menu(props: propsIState) {
-    const { disappearMenu, toggleMenu } = props
+  const { disappearMenu, toggleMenu } = props;
 
-    return (
-        <Container disappearMenu={disappearMenu}>
-            <span onClick={toggleMenu}>
-                hi
-            </span>
-        </Container>
-    )
+  return (
+    <Container disappearMenu={disappearMenu}>
+      <span onClick={toggleMenu}>hi</span>
+    </Container>
+  );
 }
 export default Menu;
 
 interface propsIState {
-    disappearMenu: boolean;
-    toggleMenu: () => void;
+  disappearMenu: boolean;
+  toggleMenu: () => void;
 }
 
 const appear = keyframes`
-    0%{ 
+    0%{
+        clip-path: polygon(0 50%, 20% 0, 100% 0, 100% 100%, 20% 100%);
         transform: translate3d(100%, 0, 0);
     }
-    90% {
-        transform: translate3d(20%, 0, 0);
-    }
-    100% {
-        transform: translate3d(0%, 0, 0);
-    }
-`;
-
-const disappear = keyframes`
-    0%{ 
-        transform: translate3d(0%, 0, 0);
-    }
-    90% {
+    10% {
         transform: translate3d(80%, 0, 0);
     }
+    90% {
+        clip-path: polygon(0 50%, 20% 0, 100% 0, 100% 100%, 20% 100%);
+        transform: translate3d(40%, 0, 0);
+    }
     100% {
+        clip-path: polygon(0 50%, 0 0, 100% 0, 100% 100%, 0 100%);
+        transform: translate3d(0%, 0, 0);
+    }
+    `;
+
+const disappear = keyframes`
+    0%{
+        clip-path: polygon(0 50%, 0 0, 100% 0, 100% 100%, 0 100%);
+        transform: translate3d(0, 0, 0);
+    }
+    10% {
+        clip-path: polygon(50% 50%, 20% 0, 100% 0, 100% 100%, 20% 100%);
+        transform: translate3d(20%, 0, 0);
+    }
+    90% {
+        clip-path: polygon(50% 50%, 20% 0, 100% 0, 100% 100%, 20% 100%);
+        transform: translate3d(60%, 0, 0);
+    }
+    100% {
+        clip-path: polygon(0 50%, 0 0, 100% 0, 100% 100%, 0 100%);
         transform: translate3d(100%, 0, 0);
     }
-`;
+    `;
 
 const Container = styled.div<{ disappearMenu: boolean }>`
-    width: 100%;
-    height: 100%;
-    
-    position: fixed;
-    top: 0;
-    left: 0;
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    background-color: ${({ theme }: { theme: ThemeIState }) => theme.primary};
-    color: ${({ theme }: { theme: ThemeIState }) => theme.fourth};
-    
-    ${(props) => props.disappearMenu ?
-        css`
-            transform: translate3d(0%, 0, 0);
-            animation: ${disappear} 0.3s linear forwards;
-        ` :
-        css`
-            transform: translate3d(-100%, 0, 0);
-            animation: ${appear} 0.3s linear forwards;
+  width: 100%;
+  height: 100%;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ theme }: { theme: ThemeIState }) => theme.primary};
+  color: ${({ theme }: { theme: ThemeIState }) => theme.fourth};
+
+  transform: translate3d(-100%, 0, 0);
+
+  ${(props) =>
+    props.disappearMenu
+      ? css`
+          animation: ${disappear} 0.3s linear forwards;
         `
-    }
-    
-    z-index: 99;
+      : css`
+          animation: ${appear} 0.3s linear forwards;
+        `}
+
+  z-index: 99;
 `;
