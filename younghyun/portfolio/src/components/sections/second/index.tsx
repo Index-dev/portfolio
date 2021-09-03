909d3d917b8a137c00ac85820b627c5f475b72fb
Eumericano <opwer032@naver.com>
2021-09-03 21:51:28 +0900

    feat: Draw second page icons

--- younghyun/portfolio/src/components/sections/second/index.tsx
+++ younghyun/portfolio/src/components/sections/second/index.tsx
@@ -1,54 +1,60 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
+import Tower from "components/icons/tower/drawing";
+import Landscape from "components/icons/tower/drawing/landscape";
 
 function SecondSection(props: propsIState) {
-    const { isPC, isTablet, secContRef, currentSecRef, currentSecScrollRef } =
-        props;
+  const { isPC, isTablet, secContRef, currentSecRef, currentSecScrollRef } =
+    props;
 
-    useEffect(() => {
-        if (secContRef.current) {
-            secContRef.current.addEventListener("scroll", () => {
-                if (
-                    (currentSecRef.current === 0 &&
-                        currentSecScrollRef.current > 0.5) ||
-                    (currentSecRef.current === 1 &&
-                        currentSecScrollRef.current < 0.5)
-                ) {
-                    console.log("drawing icon");
-                } else {
-                    console.log("remove icon");
-                }
-            });
-        }
-    });
-
-    return (
-        <ScrollSnapWrapper>
-            <Conatainer></Conatainer>
-        </ScrollSnapWrapper>
-    );
+  return (
+    <ScrollSnapWrapper>
+      <ContentConatainer></ContentConatainer>
+      <IconContainer>
+        <Tower
+          secContRef={secContRef}
+          currentSecRef={currentSecRef}
+          currentSecScrollRef={currentSecScrollRef}
+        />
+        <Landscape
+          secContRef={secContRef}
+          currentSecRef={currentSecRef}
+          currentSecScrollRef={currentSecScrollRef}
+        />
+      </IconContainer>
+    </ScrollSnapWrapper>
+  );
 }
 
 export default SecondSection;
 
 interface propsIState {
-    isPC: boolean;
-    isTablet: boolean;
-    secContRef: React.RefObject<HTMLDivElement>;
-    currentSecRef: React.MutableRefObject<number>;
-    currentSecScrollRef: React.MutableRefObject<number>;
+  isPC: boolean;
+  isTablet: boolean;
+  secContRef: React.RefObject<HTMLDivElement>;
+  currentSecRef: React.MutableRefObject<number>;
+  currentSecScrollRef: React.MutableRefObject<number>;
 }
 
 const ScrollSnapWrapper = styled.section`
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 
-    flex: none;
-    @supports (scroll-snap-align: center) {
-        scroll-snap-align: center;
-    }
+  position: relative;
 
-    border: 1px solid ${({ theme }: { theme: ThemeIState }) => theme.secondary};
+  flex: none;
+  @supports (scroll-snap-align: center) {
+    scroll-snap-align: center;
+  }
 `;
 
-const Conatainer = styled.div``;
+const ContentConatainer = styled.div``;
+
+const IconContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
