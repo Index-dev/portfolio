0d5aa9351eea636fb8d41300d72c9c606b5c8fae
Eumericano <opwer032@naver.com>
2021-09-04 09:49:44 +0900

    feat: Add images

--- younghyun/portfolio/src/components/sections/third/index.tsx
+++ younghyun/portfolio/src/components/sections/third/index.tsx
@@ -4,10 +4,66 @@ import styled from "styled-components";
 import Content from "components/sections/third/content";
 import PopUp from "components/sections/third/popup";
 
+import ssafy01 from "assets/third/ssafy/01.jpg";
+import ssafy02 from "assets/third/ssafy/02.jpg";
+import ssafy03 from "assets/third/ssafy/03.jpg";
+import ssafy04 from "assets/third/ssafy/04.jpg";
+import ssafy05 from "assets/third/ssafy/05.jpg";
+import ssafy06 from "assets/third/ssafy/06.jpg";
+import ssafy07 from "assets/third/ssafy/07.jpg";
+import ssafy08 from "assets/third/ssafy/08.jpg";
+import ssafy09 from "assets/third/ssafy/09.jpg";
+import ssafy10 from "assets/third/ssafy/10.jpg";
+import ssafy11 from "assets/third/ssafy/11.jpg";
+import ssafy12 from "assets/third/ssafy/12.jpg";
+import ssafy13 from "assets/third/ssafy/13.jpg";
+import ssafy14 from "assets/third/ssafy/14.jpg";
+import ssafy15 from "assets/third/ssafy/15.jpg";
+import ssafy16 from "assets/third/ssafy/16.jpg";
+import ssafy17 from "assets/third/ssafy/17.jpg";
+import ssafy18 from "assets/third/ssafy/18.jpg";
+import ssafy19 from "assets/third/ssafy/19.jpg";
+import ssafy20 from "assets/third/ssafy/20.jpg";
+import ssafy21 from "assets/third/ssafy/21.jpg";
+import ssafy22 from "assets/third/ssafy/22.jpg";
+import ssafy23 from "assets/third/ssafy/23.jpg";
+import ssafy24 from "assets/third/ssafy/24.jpg";
+import ssafy25 from "assets/third/ssafy/25.jpg";
+import ssafy26 from "assets/third/ssafy/26.jpg";
+
 function ThirdSection(props: propsIState) {
   const { isPC, isTablet } = props;
   const [displayBHPopUp, setDisplayBHPopUp] = useState(false);
   const [displaySSAFYPopUp, setDisplaySSAFYPopUp] = useState(false);
+
+  const ssafyImages = [
+    ssafy01,
+    ssafy02,
+    ssafy03,
+    ssafy04,
+    ssafy05,
+    ssafy06,
+    ssafy07,
+    ssafy08,
+    ssafy09,
+    ssafy10,
+    ssafy11,
+    ssafy12,
+    ssafy13,
+    ssafy14,
+    ssafy15,
+    ssafy16,
+    ssafy17,
+    ssafy18,
+    ssafy19,
+    ssafy20,
+    ssafy21,
+    ssafy22,
+    ssafy23,
+    ssafy24,
+    ssafy25,
+    ssafy26,
+  ];
 
   return (
     <ScrollSnapWrapper isPC={isPC} isTablet={isTablet}>
@@ -32,6 +88,14 @@ function ThirdSection(props: propsIState) {
         reversed={true}
         setDisplayPopUp={setDisplaySSAFYPopUp}
       />
+      {/* <Content
+        isPC={isPC}
+        isTablet={isTablet}
+        content="ETC"
+        duration={60}
+        reversed={false}
+        setDisplayPopUp={setDisplayETCPopUp}
+      /> */}
       {displayBHPopUp && (
         <PopUp
           title="BLACK HATS"
@@ -44,7 +108,7 @@ function ThirdSection(props: propsIState) {
         <PopUp
           title="SSAFY"
           duration="2020-07 ~ 2021-07"
-          images={[]}
+          images={ssafyImages}
           setDisplayPopUp={setDisplaySSAFYPopUp}
         />
       )}
